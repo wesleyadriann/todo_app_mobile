4e42b688ca1a5280a8baf6bdd6b744607b9d35a1
Wesley Souza <wesleyadriann@gmail.com>
2019-11-23 11:01:34 -0300

    Adds styled components
    Signed-off-by: Wesley Souza <wesleyadriann@gmail.com>

---
+++ src/utils/tasks.js
@@ -0,0 +1,32 @@
+export const tasks = [
+  {
+    name: 'A',
+    description: 'B',
+    type: 'C',
+    priority: 'D',
+  },
+  {
+    name: 'A',
+    description: 'B',
+    type: 'C',
+    priority: 'D',
+  },
+  {
+    name: 'A',
+    description: 'B',
+    type: 'C',
+    priority: 'D',
+  },
+  {
+    name: 'A',
+    description: 'B',
+    type: 'C',
+    priority: 'D',
+  },
+  {
+    name: 'A',
+    description: 'B',
+    type: 'C',
+    priority: 'D',
+  },
+];
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
+import {tasks} from '../../utils/tasks';
+
 const Home = () => (
   <View>
     <Text>Welcome to Native</Text>
+    {tasks.map(task => (
+      <View>
+        <Text>{task.name}</Text>
+        <Text>{task.description}</Text>
+        <Text>{task.type}</Text>
+        <Text>{task.priority}</Text>
+      </View>
+    ))}
   </View>
 );
 
